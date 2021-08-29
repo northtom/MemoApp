991924fff3339e99442039aedff8a0e71527e788
northtom <85083698+northtom@users.noreply.github.com>
2021-08-29 22:00:38 +0900

    Connect screens with react-navigation

--- App.jsx
+++ App.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import MemoListScreen from './src/screens/MemoListScreen';
-// import MemoDetailScreen from './src/screens/MemoDetailScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
-// import MemoCreateScreen from './src/screens/MemoCreateScreen';
-// import LogInScreen from './src/screens/LogInScreen';
-// import SignUpScreen from './src/screens/SingUpScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SingUpScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <MemoListScreen />
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="LogIn"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#467FD3'},
+          headerTitleStyle: {color: '#ffffff'},
+          headerTitle: 'MemoApp',
+          headerTintColor: '#ffffff',
+          headerBackTitle: 'Back',
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+        }}
+      >
+        <Stack.Screen name="MemoList" component={MemoListScreen} />
+        <Stack.Screen name="MemoDetail" component={MemoDetailScreen} />
+        <Stack.Screen name="MemoEdit" component={MemoEditScreen} />
+        <Stack.Screen name="MemoCreate" component={MemoCreateScreen} />
+        <Stack.Screen name="LogIn" component={LogInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
